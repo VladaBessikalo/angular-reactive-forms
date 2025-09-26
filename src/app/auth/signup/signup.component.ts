9e31bd7b2b2fca3775007eaa9bde1221ba9f4e57
mikhalonok <1404vladimir@gmail.com>
2025-09-26 18:58:01 +0200

    fix: add missed control to the form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {
+  FormArray,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
@@ -18,21 +19,32 @@ export class SignupComponent {
     email: new FormControl('', {
       validators: [Validators.email, Validators.required],
     }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)],
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
     }),
-    confirmPassword: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)],
-    }),
+
     firstName: new FormControl('', { validators: [Validators.required] }),
     lastName: new FormControl('', { validators: [Validators.required] }),
-    street: new FormControl('', { validators: [Validators.required] }),
-    number: new FormControl('', { validators: [Validators.required] }),
-    postalCode: new FormControl('', { validators: [Validators.required] }),
-    city: new FormControl('', { validators: [Validators.required] }),
+    address: new FormGroup({
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
+    }),
+
     role: new FormControl<
       'student' | 'teacher' | 'employee' | 'founder' | 'other'
     >('student', { validators: Validators.required }),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+    ]),
     agree: new FormControl(false, { validators: Validators.required }),
   });
 
@@ -45,9 +57,9 @@ export class SignupComponent {
   }
   get passwordIsInvalid() {
     return (
-      this.form.controls.password.touched &&
-      this.form.controls.password.dirty &&
-      this.form.controls.password.invalid
+      this.form.controls.passwords.touched &&
+      this.form.controls.passwords.dirty &&
+      this.form.controls.passwords.invalid
     );
   }
 
